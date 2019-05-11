5d280d93a9139034d0fbfe6993f2d56644f155e9
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-05-11 03:58:51 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -4,11 +4,11 @@ class RepositoriesController < ApplicationController
     resp = Faraday.get("https://api.github.com/user") do |req|
       req.headers['Authorization'] = "token #{session[:token]}"
     end
-      @username = JSON.parse(resp.body)["login"]
-      repo_resp = Faraday.get("https://api.github.com/user/repos") do |req|
-        req.headers['Authorization'] = "token #{session[:token]}"
-      end
-      @results = JSON.parse(repo_resp.body)
+    @username = JSON.parse(resp.body)["login"]
+    repo_resp = Faraday.get("https://api.github.com/user/repos") do |req|
+      req.headers['Authorization'] = "token #{session[:token]}"
+    end
+    @results = JSON.parse(repo_resp.body)
   end
 
 end
